fd40d03989f44ce4267516b825146c22d4d13a38
Richard <>
2021-06-01 22:27:50 +0000

    what happens when golfinho collies with the evil pandas, if the panda evil goes too down, collide function for when the pandas touch the walls they bounce back

--- scenes/main.js
+++ scenes/main.js
@@ -1,26 +1,65 @@
-  //Set the move speed
-  const MOVE_SPEED = 150
+//Set the move speed
+const MOVE_SPEED = 150
+const INVADER_SPEED = 500
+let CURRENT_SPEED = INVADER_SPEED
+const LEVEL_DOWN = 325
 
   //Layer
-  layer("obj", "ui", "obj")
+  layer(["obj", "ui"], "obj")
 
 //Add a level to the game - the first argument is the map and the second one the object (config)
 addLevel([
-  "!           p  p  p  p  p           &",
-  "!                                   &",
-  "!                                   &",
-  "!                                   &",
-  "!                                   &",
-  "!                                   &",
-  "!                                   &",
-  "!                                   &",
-  "!                                   &",
-  "!                                   &",
+  "!                      p  p  p  p  p                      &",
+  "!                                                         &",
+  "!                                                         &",
+  "!                                                         &",
+  "!                      p  p  p  p  p                      &",
+  "!                                                         &",
+  "!                                                         &",
+  "!                                                         &",
+  "!                      p  p  p  p  p                      &",
+  "!                                                         &",
+  "!                                                         &",
+  "!                                                         &",
+  "!                                                         &",
+  "!                                                         &",
+  "!                                                         &",
+  "!                                                         &",
+  "!                                                         &",
+  "!                                                         &",
+  "!                                                         &",
+  "!                                                         &",
+  "!                                                         &",
+  "!                                                         &",
+  "!                                                         &",
+  "!                                                         &",
+  "!                                                         &",
+  "!                                                         &",
+  "!                                                         &",
+  "!                                                         &",
+  "!                                                         &",
+  "!                                                         &",
+  "!                                                         &",
+  "!                                                         &",
+  "!                                                         &",
+  "!                                                         &",
+  "!                                                         &",
+  "!                                                         &",
+  "!                                                         &",
+  "!                                                         &",
+  "!                                                         &",
+  "!                                                         &",
+  "!                                                         &",
+  "!                                                         &",
+  "!                                                         &",
+  "!                                                         &",
+  "!                                                         &",
+  "!                                                         &",
 ], {
   //The width and the height are the values for every object (character) in the map e.g. x
   width: 30,
   height: 22,
-  "p" : [sprite("panda-evil"), scale(0.1)],
+  "p" : [sprite("panda-evil"), scale(0.1), "panda-evil"],
   "!" : [sprite("wall"), "left-wall"],
   "&" : [sprite("wall"), "right-wall"],
 })
@@ -29,7 +68,8 @@ addLevel([
 const player = add([
   sprite("golfinho"), scale(0.1),
   pos(width() / 2, height() / 2),
-  origin("center")
+  // origin("center")
+  pos(900, 1000)
   ])
 
   //Assign keystrokes to the player
@@ -78,3 +118,37 @@ timer.action(() => {
     go("lose");
   }
 })
+
+//Get the Panda Evil moving right and left
+action("panda-evil", (p) => {
+  p.move(CURRENT_SPEED, 0)
+})
+
+//Collide function for when the pandas touch the walls they bounce back
+collides("panda-evil", "right-wall", () => {
+  CURRENT_SPEED = -INVADER_SPEED
+  every("panda-evil", (p) => {
+    p.move(0, LEVEL_DOWN)
+  })
+})
+
+collides("panda-evil", "left-wall", () => {
+  CURRENT_SPEED = INVADER_SPEED
+  every("panda-evil", (p) => {
+    p.move(0, LEVEL_DOWN)
+  })
+})
+
+//What happens when golfinho collides with the evil pandas
+player.overlaps("panda-evil", () => {
+  go("lose", { score: score.value})
+})
+
+//if the evil panda goes too LEVEL_DOWN
+action("panda-evil", (p) => {
+  if(p.pos.y >= height()/2){
+    go("lose", {score: score.value})
+  }
+})
+
+//Spawning bullets
